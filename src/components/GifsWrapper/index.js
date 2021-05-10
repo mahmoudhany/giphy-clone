f5f33a17c83766793c260ab0f5277b49c86641bf
Mahmoud <ma7moud.hany158@gmail.com>
2021-05-10 09:37:42 +0200

    fix double loading issue and clean API calls

--- src/components/GifsWrapper/index.js
+++ src/components/GifsWrapper/index.js
@@ -1,94 +1,57 @@
-import React, { useCallback, useEffect, useState } from 'react'
 import InfiniteScroll from 'react-infinite-scroll-component';
-// import axios from '../../API'
-import Axios from 'axios';
 import Gif from '../Gif';
 import Loading from '../Loading';
 import './gifWrapper.scss'
 
-export default function GifsWrapper({ endpoint, limit, keyword = null }) {
-
-  const [gifs, setGifs] = useState({ list: [] })
-  const [loading, setLoading] = useState(false)
-  const [totalCount, setTotalCount] = useState(0)
-  const [offset, setOffset] = useState(0)
-  const apiKey = process.env.REACT_APP_API_KEY
-
-
-  useEffect(() => {
-    // let source = Axios.CancelToken.source()
-
-    const fetchGifs = async () => {
-      setLoading(true)
-      try {
-        const response = await Axios.get(
-          `https://api.giphy.com/v1/gifs/${endpoint}?api_key=${apiKey}${keyword ? ('&q=' + keyword) : ''}&limit=${limit}&offset=${offset}`,
-          //    {
-          //   cancelToken: source.token
-          // }
-        )
-        setTotalCount(response.data.pagination.total_count)
-        setGifs(prev => ({ list: [...prev.list, ...response.data.data] }))
-        setLoading(false)
-
-      } catch (error) {
-        setLoading(false)
-        // if (Axios.isCancel(error)) {
-        //   console.log('cancel error');
-        // } else {
-        console.log(error.message)
-        // }
-
-      }
-    }
-    fetchGifs()
-    return () => {
-      // mounted = false
-      // source.cancel()
-    }
-  }, [endpoint, limit, offset, apiKey, keyword])
+const GifsWrapper = ({ data, isLoading, changeOffset, title }) => {
 
   const renderList = () => {
     return (
-      totalCount > 0 &&
       <InfiniteScroll
-        dataLength={gifs.list.length} //This is important field to render the next data
-        next={() => setOffset(prev => limit + prev)}
-        hasMore={totalCount > offset}
+        //This is important field to render the next data
+        dataLength={data.length}
+        next={changeOffset}
+        hasMore={true}
         className='list'
-        loader={loading && <Loading />}
+        // loading indicator
+        loader={isLoading && <Loading />}
+        // scroll to top
+        id="scrollableDiv"
+        scrollableTarget="scrollableDiv"
         endMessage={
-          <p style={{ textAlign: 'center' }}>
+          < p style={{ textAlign: 'center' }}>
             <b>Yay! You have seen it all</b>
           </p>
         }
+        style={{ overflow: 'hidden' }}
       >
         {
-          gifs.list.map(({ id, title, images: { downsized_large } }, index) => {
-            if (index > 0 && (gifs.list[index - 1].id !== id)) {
-              return <Gif
-                key={id}
-                imageUrl={downsized_large.url}
-                title={title}
-                height={downsized_large.height}
-              />
-            }
+          // render single gif component
+          data.map(({ id, title, images: { downsized_large } }) => {
+            return <Gif
+              key={id}
+              imageUrl={downsized_large.url}
+              title={title}
+              height={downsized_large.height}
+            />
           }
           )
         }
       </InfiniteScroll >
-      //     :
-      // <p className='text-center'>No GIFs found for: {keyword}</p>
     )
   }
   return (
     <div className='container'>
       <div className="list-wrapper">
-        {keyword && <h1><span>{keyword}</span></h1>}
+        {title ? <h1><span>{title}</span></h1> : ''}
         {
-          renderList()
+          data.length > 0 ?
+            renderList() :
+            <p className='text-center'> No GIFs found for: {title}</p>
         }
       </div>
     </div >
   )
 }
+
+export default GifsWrapper
--- src/page/Search.js
+++ src/page/Search.js
@@ -1,14 +1,71 @@
-import React from 'react'
+import React, { useCallback, useEffect, useState } from 'react'
+import { fetchGifsAction } from '../API/api';
 import GifsWrapper from '../components/GifsWrapper'
 
-export default function Search(props) {
+const Search = (props) => {
+  const [state, setState] = useState({
+    data: [],
+    count: 0,
+    loading: false,
+    limit: 5,
+    offset: 0,
+    keyword: props.match.params.q
+  });
+
+  // fetch gifs
+  const fetchData = useCallback(async () => {
+    // setting initial state
+    setState(prev => ({ ...prev, loading: true }))
+    // get response data
+    const { success, data, count, message } = await fetchGifsAction({
+      offset: state.offset,
+      endpoint: 'search',
+      keyword: props.match.params.q !== state.keyword ? props.match.params.q : state.keyword,
+      limit: state.limit
+    });
+
+    // setting new state 
+    if (success) {
+      setState(prev => ({
+        ...prev,
+        data: props.match.params.q !== state.keyword ? data :
+          [...state.data, ...data],
+        count,
+        loading: false,
+        keyword: props.match.params.q !== state.keyword ?
+          props.match.params.q : state.keyword
+      })
+      );
+    }
+    // handle error message 
+    if (!success) {
+      alert(message)
+      setState(prev => ({ ...prev, loading: false }))
+    };
+  }, [state.offset, props.match.params.q]);
+
+  useEffect(() => {
+    fetchData();
+  },
+    [fetchData]);
+
   return (
     <>
-      <GifsWrapper
-        endpoint='search'
-        limit={10}
-        keyword={props.match.params.q}
-      />
+      {/* render gifs list component safely */}
+      {state.data && <GifsWrapper
+        title={state.keyword}
+        isLoading={state.loading}
+        limit={state.limit}
+        data={state.data}
+        keyword={state.keyword}
+        // change offset to allow infinite scrolling
+        changeOffset={() => setState(prev => ({
+          ...prev,
+          offset: state.limit + state.offset
+        }))}
+      />}
     </>
   )
 }
+
+export default Search
